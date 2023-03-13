29c8aee4a39c9296b2c723d381e38bef26d64d1f
Signe Grosberga <signe.grosberga@va.lv>
2023-03-13 05:15:23 +0200

    ze markerz i ze lizt
    - ze marker only shows where u are at ze moment
    - theres a list for api history but not yet connected

--- app/src/main/java/com/example/mpi2pd2_real/ApiActivity.kt
+++ app/src/main/java/com/example/mpi2pd2_real/ApiActivity.kt
@@ -1,10 +1,15 @@
 package com.example.mpi2pd2_real
 
+import android.content.Context
 import android.content.Intent
+import android.database.DataSetObserver
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
+import android.widget.*
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 
 class ApiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +28,54 @@ class ApiActivity : AppCompatActivity() {
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+    class ApiActivity : AppCompatActivity() {
+
+        private lateinit var listView: ListView
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_api)
+
+            listView = findViewById(R.id.list_view)
+
+            // Make API request
+            val queue = Volley.newRequestQueue(this)
+            val url = "https://jsonplaceholder.typicode.com/todos"
+            val request = JsonArrayRequest(
+                Request.Method.GET, url, null,
+                { response ->
+                    // Convert JSON array to list of strings
+                    val data = mutableListOf<String>()
+                    for (i in 0 until response.length()) {
+                        val item = response.getJSONObject(i)
+                        data.add(item.getString("title"))
+                    }
+
+                    // Display data in ListView
+                    val adapter = MyAdapter(this, data)
+                    listView.adapter = adapter
+                },
+                { error ->
+                    Toast.makeText(this, "Error: ${error.message}", Toast.LENGTH_SHORT).show()
+                })
+
+            queue.add(request)
+        }
+    }
+    class MyAdapter(context: Context, private val data: List<String>) : ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, data) {
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            var view = convertView
+            if (view == null) {
+                view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false)
+            }
+
+            val item = getItem(position)
+            view?.findViewById<TextView>(android.R.id.text1)?.text = item
+
+            return view!!
         }
     }
 
--- app/src/main/java/com/example/mpi2pd2_real/MapsActivity.kt
+++ app/src/main/java/com/example/mpi2pd2_real/MapsActivity.kt
@@ -4,16 +4,22 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.android.volley.VolleyLog.TAG
 import com.example.mpi2pd2_real.databinding.ActivityMapsBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
 
 @Suppress("DEPRECATION")
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -26,6 +32,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        Places.initialize(applicationContext, "AIzaSyBcfcQcxtbavmWNRKb5ZpvlIXiN2X8F5CM")
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -77,6 +85,33 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 val latLng = LatLng(location.latitude, location.longitude)
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
                 mMap.setOnMyLocationChangeListener(null) // remove listener after first update
+
+                // Add a marker on the user's location
+                mMap.addMarker(MarkerOptions().position(latLng).title("My Location"))
+
+                // Use the Places SDK to search for nearby points of interest
+                val placesClient = Places.createClient(this)
+                val request = FindCurrentPlaceRequest.builder(listOf(Place.Field.NAME, Place.Field.ADDRESS))
+                    .build()
+                placesClient.findCurrentPlace(request).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val likelyPlaces = task.result
+                        for (place in likelyPlaces?.placeLikelihoods ?: emptyList()) {
+                            val latLng = place.place.latLng
+                            if (latLng != null) {
+                                mMap.addMarker(
+                                    MarkerOptions()
+                                        .position(latLng)
+                                        .title(place.place.name)
+                                        .snippet(place.place.address)
+                                )
+                            }
+                        }
+
+                    } else {
+                        Log.e(TAG, "Exception: ${task.exception}")
+                    }
+                }
             }
         }
 
@@ -85,6 +120,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.uiSettings.isCompassEnabled = true
         mMap.uiSettings.isMyLocationButtonEnabled = true
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_maps, menu)
         return true
@@ -100,3 +136,5 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 }
+
+
