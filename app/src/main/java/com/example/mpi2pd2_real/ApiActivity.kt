44c21408330cfa6ed060fe9288ebdcb826ec5005
Krista <kristabriede01@gmail.com>
2023-03-13 18:13:31 +0200

    ziema ak vai si ziemina

--- app/src/main/java/com/example/mpi2pd2_real/ApiActivity.kt
+++ app/src/main/java/com/example/mpi2pd2_real/ApiActivity.kt
@@ -5,12 +5,72 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.os.AsyncTask
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class ApiActivity : AppCompatActivity() {
+
+    private lateinit var listView: ListView
+    private lateinit var adapter: ArrayAdapter<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_api)
+
+        listView = findViewById(R.id.list_view)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, mutableListOf())
+        listView.adapter = adapter
+
+        FetchDataTask().execute("https://animechan.vercel.app/api/quotes")
     }
+    inner class FetchDataTask : AsyncTask<String, Void, List<String>>() {
+
+        override fun doInBackground(vararg urls: String): List<String> {
+            val url = URL(urls[0])
+            val connection = url.openConnection() as HttpURLConnection
+            connection.connectTimeout = 5000
+            connection.readTimeout = 5000
+            connection.requestMethod = "GET"
+            connection.doInput = true
+            connection.connect()
+
+            val responseCode = connection.responseCode
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                val inputStream = connection.inputStream
+                val reader = BufferedReader(InputStreamReader(inputStream))
+                val response = StringBuilder()
+                var line: String?
+                while (reader.readLine().also { line = it } != null) {
+                    response.append(line)
+                }
+                reader.close()
+                inputStream.close()
+
+                val jsonArray = JSONArray(response.toString())
+                val listData = mutableListOf<String>()
+                for (i in 0 until jsonArray.length()) {
+                    val jsonObject = jsonArray.getJSONObject(i)
+                    listData.add(jsonObject.getString("character")+ " from " + jsonObject.getString("anime") + " said: " + jsonObject.getString("quote"))
+                }
+                return listData
+            }
+            return emptyList()
+        }
+
+        override fun onPostExecute(result: List<String>) {
+            adapter.addAll(result)
+            adapter.notifyDataSetChanged()
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_api, menu)
         return true
